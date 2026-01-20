8972c425e8cdf516dc2f6dd2b625a85cd8a8b048
gotlr98 <gotlr98@naver.com>
2026-01-20 14:55:19 +0900

    generate spring-kakao-map project

---
+++ spring-kakao-map/src/main/java/com/example/spring_kakao_map/SpringKakaoMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_kakao_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKakaoMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKakaoMapApplication.class, args);
+	}
+
+}
---
+++ spring-kakao-map/src/test/java/com/example/spring_kakao_map/SpringKakaoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_kakao_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKakaoMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
